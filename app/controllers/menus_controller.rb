9bcc2a8c2c7e3281f0347c3eb61766f4d5be7edc
Lukasz <lukaszkkoziel@gmail.com>
2020-05-25 13:08:48 +0900

    menus and restaurants controllers created

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,33 @@
 class MenusController < ApplicationController
+   before_action :set_menu, only: [:show, :edit, :update]
+  def show
+
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      redirect_to menu_path(@menu)
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    @menu.update(menu_params)
+    redirect_to menu_path(@menu)
+  end
+
+  private
+
+  def set_menu
+    @menu = Menu.find(params[:id])
+  end
+
+  def menu_params
+    params.require(:menu).permit(:currency)
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,23 @@
 class RestaurantsController < ApplicationController
+  def show
+    @restaurant = Resturant.find(params[:id])
+    @menu = Menu.new
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Resturant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@resturant)
+    else
+      render :new
+  end
+
+  private
+    def restaurant_params
+      params.require(:restaurant).permit(:address, :name, :phone_number, :cuisine)
+    end
 end
