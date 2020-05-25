2094e96aa9c326de00053393682407df3598a87b
Momotaro Nakamura <mrnakamura721@gmail.com>
2020-05-25 13:07:43 +0900

    Updated validations for photo

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,6 +2,6 @@ class Menu < ApplicationRecord
   belongs_to :restaurant
   has_many :entries
   has_many :dishes, through: :entries
-  validates :currency, :photo, presence: true
+  validates :currency, :photos, presence: true
   has_many_attached :photos
 end
