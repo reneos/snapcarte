74d6bc09e327040c1ce3147c640501728a4efbac
Nicole Wong <nrewong@gmail.com>
2020-05-25 12:11:06 +0900

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:show, :new, :create]
+  resources :menus, only: [:show, :create, :edit, :update]
+  resources :dishes, only: [:create, :update]
+  resources :entries, only: :create
 end
