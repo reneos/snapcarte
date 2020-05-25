95e78ec0c1b770c5d5ede4e6cd6bb60de6290886
Nicole Wong <nrewong@gmail.com>
2020-05-25 16:50:48 +0900

    create restaurant form w/ menu photo upload

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,24 @@
 class RestaurantsController < ApplicationController
+  def new
+    @restaurant = Restaurant.new
+    @restaurant.menus.build
+    authorize @restaurant
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user = current_user
+    authorize @restaurant
+    if @restaurant.save
+      redirect_to edit_restaurant_menu_path(@restaurant, @restaurant.menus.first)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :cuisine, menus_attributes: [:currency, :photos])
+  end
 end
