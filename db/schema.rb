7f59990bba2c06618006d5945a48fcbbbe932484
Nicole Wong <nrewong@gmail.com>
2020-05-28 13:27:22 +0900

    removed old migration

--- db/schema.rb
+++ db/schema.rb
@@ -55,16 +55,6 @@ ActiveRecord::Schema.define(version: 2020_05_28_040210) do
     t.index ["restaurant_id"], name: "index_menus_on_restaurant_id"
   end
 
-  create_table "operation_times", force: :cascade do |t|
-    t.bigint "restaurant_id", null: false
-    t.time "start_time"
-    t.time "end_time"
-    t.string "day"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["restaurant_id"], name: "index_operation_times_on_restaurant_id"
-  end
-
   create_table "restaurants", force: :cascade do |t|
     t.string "address"
     t.string "name"
@@ -93,6 +83,5 @@ ActiveRecord::Schema.define(version: 2020_05_28_040210) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "dishes", "menus"
   add_foreign_key "menus", "restaurants"
-  add_foreign_key "operation_times", "restaurants"
   add_foreign_key "restaurants", "users"
 end
