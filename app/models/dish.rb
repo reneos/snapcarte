6f47bdea4f1d91fdc8ccb0d91e55859c088378e6
Nicole Wong <nrewong@gmail.com>
2020-05-28 13:44:24 +0900

    add price formatting usd

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,10 @@
 class Dish < ApplicationRecord
   validates :name, :price, presence: true
   belongs_to :menu
+
+  def pricetag
+    float_price = "#{price.to_f / 100}"
+    float_price += float_price.match?(/\.\d$/) ? "0" : ""
+    "$#{float_price}"
+  end
 end
