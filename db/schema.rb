977708361cf8f511b31d09e14ae2faef8c592e1e
Nicole Wong <nrewong@gmail.com>
2020-05-28 16:12:16 +0900

    change price to float

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_045857) do
+ActiveRecord::Schema.define(version: 2020_05_28_064724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2020_05_28_045857) do
 
   create_table "dishes", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.float "price"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
