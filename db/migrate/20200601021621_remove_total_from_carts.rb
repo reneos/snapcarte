666a5ee02bf513619d477d24b699f79e3bd78abf
Nicole Wong <nrewong@gmail.com>
2020-06-02 12:03:27 +0900

    cleaned up migrations

--- db/migrate/20200601021621_remove_total_from_carts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTotalFromCarts < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :carts, :total, :integer
-  end
-end
--- db/migrate/20200601021746_add_confirmed_to_carts.rb
+++
@@ -1,5 +0,0 @@
-class AddConfirmedToCarts < ActiveRecord::Migration[6.0]
-  def change
-    add_column :carts, :confirmed, :integer
-  end
-end
