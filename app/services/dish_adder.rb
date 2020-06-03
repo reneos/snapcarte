dd7b5ae55c94a7de0c476c354de1648ad32a6657
Nicole Wong <nrewong@gmail.com>
2020-06-03 11:33:00 +0900

    change timestamp column

--- app/services/dish_adder.rb
+++ app/services/dish_adder.rb
@@ -2,13 +2,14 @@ class DishAdder
   def self.call(dish_array, menu)
     dish_array.each do |line|
       name = line[0]
-      price = ((line[1].to_f) * 100).to_i
+      price_cents = ((line[1].to_f) * 100).to_i
       Dish.create(
         name: name,
-        price: price,
+        price_cents: price_cents,
         menu: menu
         )
     end
   end
 
 end
+8.60
