c6afd8071717811060ab435154c333950a0771a4
Nicole Wong <nrewong@gmail.com>
2020-06-05 11:43:55 +0900

    preview restaurant button now submits then redirects

--- app/javascript/controllers/dishes_controller.js
+++ app/javascript/controllers/dishes_controller.js
@@ -1,15 +1,19 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "submitButton", "previewButton"]
+  static targets = [ "submitButton", "previewButton", "hiddenField"]
 
 
 
   submitOnEnter(e) {
     if (e.key === 'Enter'){
-      e.preventDefault()
-      this.submitButtonTarget.click()
+      e.preventDefault();
+      this.submitButtonTarget.click();
     }
   }
-
+  submitRedirect(e) {
+    e.preventDefault();
+    this.hiddenFieldTarget.value = "true";
+    this.submitButtonTarget.click();
+  }
 }
